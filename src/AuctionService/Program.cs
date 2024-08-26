bba54cc5c4d1fb37d60e2a5c6a5849e9da222ca5
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-08-26 11:27:58 +0700

    add a postgres database server

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>(opt=>{
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConntection"));
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
