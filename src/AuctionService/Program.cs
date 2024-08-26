d32c6127d26896ecbe3dca5d9a11279266b42c5a
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-08-26 14:16:48 +0700

    creating seed data

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,6 +1,7 @@
 using AuctionService.Data;
 using Microsoft.EntityFrameworkCore;
 
+//builder khởi tạo một ứng dụng WebApplication, nơi các dịch vụ(services) như controller và DbContext được thêm vào
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,5 +16,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//khởi tạo cơ sở dữ liệu và seed dữ liệu ban đầu nếu cần thiết
+try
+{
+    DbInitializer.InitDb(app);
+} catch(Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
