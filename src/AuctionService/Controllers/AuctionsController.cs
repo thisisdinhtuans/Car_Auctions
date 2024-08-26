340af02da316fede01d5e170ba26081156cc0131
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-08-26 22:59:54 +0700

    Ađ an api controller

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,41 @@
+using System;
+using AuctionService.Data;
+using AuctionService.DTOs;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+        private readonly AuctionDbContext _context;
+        private readonly IMapper _mapper;
+        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }   
+
+        [HttpGet]
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+        {
+            var auctions=await _context.Auctions
+                .Include(x=>x.Item)
+                .OrderBy(x=>x.Item.Make)
+                .ToListAsync();
+            return _mapper.Map<List<AuctionDto>>(auctions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+        {
+            var auction = await _context.Auctions
+                .Include(x=>x.Item)
+                .FirstOrDefaultAsync();
+            
+            if(auction==null) return NotFound();
+            return _mapper.Map<AuctionDto>(auction);
+        }
+}
