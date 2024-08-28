575ad081d5ed099a195d37227d356c8614a0e892
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-08-28 14:46:59 +0700

    pagination the result

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -10,9 +10,9 @@ namespace SearchService.Controllers;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async  Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    public async  Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber=1, int pageSize=4)
     {
-        var query=DB.Find<Item>();
+        var query=DB.PagedSearch<Item>();
 
         query.Sort(x=>x.Ascending(a=>a.Make));
 
@@ -21,8 +21,15 @@ public class SearchController : ControllerBase
             query.Match(Search.Full, searchTerm).SortByTextScore();
         }
 
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
+
         var result=await query.ExecuteAsync();
 
-        return result;
+        return Ok(new {
+            results=result.Results,
+            pageCount=result.PageCount,
+            totalCount=result.TotalCount
+        });
     }
 }
