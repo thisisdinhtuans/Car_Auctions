59f1df5efa3c54ca68cbc9949909e70d04983199
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-08-29 11:51:12 +0700

    making our Http communication resilient

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -9,27 +9,27 @@ namespace SearchService.Data;
 
 public class DbInitializer
 {
-    public static async Task InitDb(WebApplication app)
+public static async Task InitDb(WebApplication app)
     {
-        await DB.InitAsync("SearchDb", MongoClientSettings
-            .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+        await DB.InitAsync("SearchDb",
+            MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
 
         await DB.Index<Item>()
-            .Key(x=>x.Make, KeyType.Text)
-            .Key(x=>x.Model, KeyType.Text)
-            .Key(x=>x.Color, KeyType.Text)
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
             .CreateAsync();
 
         var count = await DB.CountAsync<Item>();
 
-        using var scope=app.Services.CreateScope();
+        using var scope = app.Services.CreateScope();
 
-        var httpClient=scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
 
-        var items=await httpClient.GetItemsForSearchDb();
+        var items = await httpClient.GetItemsForSearchDb();
 
-        Console.WriteLine(items.Count+" returned from the auction service");
+        Console.WriteLine(items.Count + " returned from auction service");
 
-        if(items.Count>0) await DB.SaveAsync(items);
+        if (items.Count > 0) await DB.SaveAsync(items);
     }
 }
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,7 +1,7 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
-using SearchService.Models;
 using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,21 +9,30 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-try
-{
-    await DbInitializer.InitDb(app);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-}
+app.Lifetime.ApplicationStarted.Register(async()=>{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
