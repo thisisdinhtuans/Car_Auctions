4a3ddefb5f6002f46ddffda77cefc35202e3c40b
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-08-31 20:32:59 +0700

    Consuming fault queues

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,3 +1,4 @@
+using AuctionService.Consumers;
 using AuctionService.Data;
 using MassTransit;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +23,9 @@ builder.Services.AddMassTransit(x=>
         o.UsePostgres();
         o.UseBusOutbox();
     });
+
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
 
     x.UsingRabbitMq((context, cfg)=>
     {
