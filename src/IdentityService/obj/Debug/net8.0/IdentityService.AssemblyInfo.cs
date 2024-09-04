f3cf535eeee0f639290f8ec6dbcbe69e673889c1
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-09-04 23:09:21 +0700

    Adding client credentials to allow clients to request a token

--- src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
+++ src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e7cd86577fb827f2811d9eda6c62e894206e42b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
