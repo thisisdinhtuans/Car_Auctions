9e7cd86577fb827f2811d9eda6c62e894206e42b
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-09-04 21:27:13 +0700

    add a register page 2

--- src/IdentityService/Pages/Account/Register/Index.cshtml.cs
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -8,8 +8,53 @@ namespace IdentityService.Pages.Account.Register
     [AllowAnonymous]
     public class Index : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+        public Index(UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public RegisterViewModel Input{ get; set; }
+
+        [BindProperty]
+        public bool RegisterSuccess { get; set; }
+
+        public IActionResult OnGet(string returnUrl)
+        {   
+            Input=new RegisterViewModel
+            {
+                returnUrl=returnUrl,
+            };
+
+            return Pages();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(Input.Button!="register") return Redirect("~/");
+
+            if(ModelState.IsValid)
+            {
+                var user=new ApplicationUser{
+                    UserName=Input.Username,
+                    Email=Input.Email,
+                    EmailConfirmed=true
+                };
+
+                var result=await _userManager.CreateAsync(user, Input.Password);
+
+                if(result.Succed)
+                {
+                    await _userManager.AddClaimsAsync(user, new Claims[]
+                    {
+                        new Claims(JwtClaimType.Name, Input.FullName)
+                    });
+                    RegisterSuccess=true;
+                }
+            }
+
+            return Pages();
         }
     }
 }
