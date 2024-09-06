18e31e6065fa8b788e1ff3677ee1733caaeb8d2b
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-09-06 16:29:12 +0700

    Adding the Reverse Proxy configuration

--- src/GatewayService/obj/Debug/net8.0/GatewayService.AssemblyInfo.cs
+++ src/GatewayService/obj/Debug/net8.0/GatewayService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee76ae9c90bc596ed74627218c7112544c8b672")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2d704c1256bf4a510b0ddbbbe6ae54f0d1e35e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
