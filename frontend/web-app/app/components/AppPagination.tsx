e8b8fb160e256e6b0b44a568c974dbff66a3d886
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-09-18 10:54:15 +0700

    Using server functions in client components

--- frontend/web-app/app/components/AppPagination.tsx
+++ frontend/web-app/app/components/AppPagination.tsx
@@ -1,19 +1,19 @@
 'use client'
 
 import { Pagination } from 'flowbite-react'
-import React, { useState } from 'react'
+import React from 'react'
 
 type Props = {
     currentPage:number
     pageCount:number
+    pageChanged: (page: number)=>void;
 }
 
-export default function AppPagination({currentPage, pageCount}:Props) {
-    const [pageNumber, setPageNumber]=useState(currentPage);
+export default function AppPagination({currentPage, pageCount, pageChanged}:Props) {
   return (
     <Pagination 
-        currentPage={pageNumber}
-        onPageChange={e=>setPageNumber(e)}
+        currentPage={currentPage}
+        onPageChange={e=>pageChanged(e)}
         totalPages={pageCount}
         layout='pagination'
         showIcons={true}
