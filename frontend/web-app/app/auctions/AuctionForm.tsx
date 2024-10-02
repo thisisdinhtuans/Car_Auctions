3cffa15661f32f8d293f246942289864d1cc5cae
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-10-02 11:24:36 +0700

    Creating a reuable date input

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,13 +1,23 @@
 'use client'
 
 import { Button, TextInput } from 'flowbite-react';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FieldValues, useForm } from 'react-hook-form';
 import Input from '../components/Input';
+import DateInput from '../components/DateInput';
 
 export default function AuctionForm() {
   const {control, handleSubmit, setFocus,
-    formState: {isSubmitting, isValid, isDirty, errors}}=useForm();
+    formState: {isSubmitting, isValid, isDirty, errors}}=useForm({
+      //bấm vào nó sẽ yêu cầu kiểu như là cái này không được để trống
+      mode:'onTouched'
+    });
+
+    //tức là lúc đầu vào trang thì con trỏ nó sẽ ở thằng make luôn(trỏ vào thằng make đầu tiên luôn)
+    useEffect(()=> {
+      setFocus('make');
+    },[setFocus])
+
     function onSubmit(data: FieldValues) {
       console.log(data);
     }
@@ -15,6 +25,26 @@ export default function AuctionForm() {
     <form className='flex flex-col mt-3' onSubmit={handleSubmit(onSubmit)}>
       <Input label='Make' name='make' control={control} rules={{required: 'Make is required'}}/>
       <Input label='Model' name='model' control={control} rules={{required: 'Model is required'}}/>
+      <Input label='Color' name='color' control={control} rules={{required: 'Color is required'}}/>
+
+      <div className='grid grid-cols-2 gap-3'>
+        <Input label='Year' name='year' control={control} type='number' rules={{required: 'Year is required'}}/>
+        <Input label='Mileage' name='mileage' control={control} type='number' rules={{required: 'Mileage is required'}}/>
+      </div>
+
+      <Input label='Image URL' name='imageUrl' control={control} rules={{required: 'Image URL is required'}}/>
+
+      <div className='grid grid-cols-2 gap-3'>
+        <Input label='Reserve Price (enter 0 if no reserve)' name='reservePrice' control={control} type='number' rules={{required: 'Reserve price is required'}}/>
+        <DateInput 
+          label='Auction end date/time' 
+          name='auctionEnd' 
+          dateFormat='dd MMMM yyyy h:mm a'
+          showTimeSelect
+          control={control} 
+          type='date' 
+          rules={{required: 'Auction end date is required'}}/>
+      </div>
 
       <div className='flex justify-between'>
         <Button outline color='gray'>Cancel</Button>
