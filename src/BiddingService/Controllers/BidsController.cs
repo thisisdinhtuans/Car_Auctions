1281d9ebabebf094b8e6df4b9c0284751646fe3b
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-10-08 10:13:50 +0700

    Adding a gRPC client

--- src/BiddingService/Controllers/BidsController.cs
+++ src/BiddingService/Controllers/BidsController.cs
@@ -2,6 +2,7 @@ using System;
 using AutoMapper;
 using BiddingService.DTOs;
 using BiddingService.Models;
+using BiddingService.Services;
 using Contracts;
 using MassTransit;
 using Microsoft.AspNetCore.Authorization;
@@ -16,11 +17,13 @@ public class BidsController:ControllerBase
 {
     private IMapper _mapper;
     private readonly IPublishEndpoint _publishEndpoint;
+    private readonly GrpcAuctionClient _grpcClient;
 
-    public BidsController(IMapper mapper, IPublishEndpoint publishEndpoint)
+    public BidsController(IMapper mapper, IPublishEndpoint publishEndpoint, GrpcAuctionClient grpcClient)
     {
         _mapper=mapper;
         _publishEndpoint = publishEndpoint;
+        _grpcClient = grpcClient;
     }
     [Authorize]
     [HttpPost]
@@ -30,8 +33,9 @@ public class BidsController:ControllerBase
 
         if(auction == null)
         {
-            //TODO: check with auction service if that has auction
-            return NotFound();
+            auction=_grpcClient.GetAuction(auctionId);
+
+            if(auction==null) return BadRequest("Cannot accept bids on this auction at the time");
         } 
 
         if(auction.Seller == User.Identity?.Name)
